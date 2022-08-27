234c1c5c06b010a9885a3be5cea092736ac3ffe5
susu3131 <anny81327@gmail.com>
2022-08-27 10:47:48 +0800

    首頁100%

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,52 +1,39 @@
-$(function() {
-  console.log('Hello Bootstrap5');
-});
+$(document).ready(function () {
 
+  $(".index-dc").click(function (e) {
+    event.preventDefault();
+    $(".index-choose").toggle();
+
+  });
+})
+
+
+//輪播
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 2,
   spaceBetween: 10,
   loop: true,
+  centeredSlides: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  breakpoints: { 
-    450: {  //当屏幕宽度大于等于375
+  breakpoints: {
+    450: { //当屏幕宽度大于等于375
       slidesPerView: 3,
       spaceBetween: 15,
     },
-    576: {  //当屏幕宽度大于等于375
+    576: { //当屏幕宽度大于等于375
       slidesPerView: 4,
       spaceBetween: 20,
     },
-    767: {  //当屏幕宽度大于等于375
+    767: { //当屏幕宽度大于等于375
       slidesPerView: 5,
       spaceBetween: 30,
     },
-    992: {  //当屏幕宽度大于等于375
+    992: { //当屏幕宽度大于等于375
       slidesPerView: 6,
       spaceBetween: 30,
     },
   }
 });
-
-
-// var swiper = new Swiper(".mySwiper", {
-//   slidesPerView: 3,
-//   spaceBetween: 30,
-//   slidesPerGroup: 3,
-//   zoom: true,
-//   loop: true,
-//   loopFillGroupWithBlank: true,
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-
-// });
-
-
