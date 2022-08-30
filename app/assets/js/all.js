45b96aa8e9bf568c97b36bfb2059a7d06b05ae26
susu3131 <anny81327@gmail.com>
2022-08-31 00:30:11 +0800

    pet頁面完成100%

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -67,3 +67,37 @@ var swiper = new Swiper(".Swiper-pet", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+
+//pet 影片輪播
+
+var swiper = new Swiper(".Swiper-video", {
+  slidesPerView: 1,
+  spaceBetween:30,
+  autoHeight: true,
+
+  loop: true,
+  centeredSlides: true,
+  navigation: {
+    nextEl: ".left",
+    prevEl: ".right",
+  },
+  breakpoints: {
+
+    767: { //当屏幕宽度大于等于375
+      slidesPerView: 2,
+    },
+    992: { //当屏幕宽度大于等于375
+      slidesPerView: 2,
+    },
+    1200: { //当屏幕宽度大于等于375
+      slidesPerView: 3,
+    },
+
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+});
